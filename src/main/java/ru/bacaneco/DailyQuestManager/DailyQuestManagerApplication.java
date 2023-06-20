c5e15c83a64f984b42f917e026d5294ce87023f5
bacaneko <bacaneco@mail.ru>
2023-06-20 18:10:16 +0300

    Initial commit

---
+++ src/main/java/ru/bacaneco/DailyQuestManager/DailyQuestManagerApplication.java
@@ -0,0 +1,16 @@
+package ru.bacaneco.DailyQuestManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyQuestManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyQuestManagerApplication.class, args);
+
+
+
+	}
+
+}
---
+++ src/test/java/ru/bacaneco/DailyQuestManager/DailyQuestManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bacaneco.DailyQuestManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyQuestManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
